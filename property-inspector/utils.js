ecfbe3802e8d6241f881cf7adc0d3fdaa4fce638
Sean Smith <sean@wwsean08.com>
2020-10-30 18:05:13 -0700

    Extract common javascript functions to their own file deduplication ftw

---
+++ property-inspector/utils.js
@@ -0,0 +1,58 @@
+function UpdateSettings() {
+    if (websocket) {
+        const json = {
+            "action": actionInfo.action,
+            "event": "setSettings",
+            "context": uuid,
+            "payload": actionInfo.payload.settings
+        };
+        websocket.send(JSON.stringify(json));
+    }
+}
+
+// our method to pass values to the plugin
+function sendValueToPlugin(payload) {
+    if (websocket) {
+        const json = {
+            "action": actionInfo.action,
+            "event": "sendToPlugin",
+            "context": uuid,
+            "payload": payload
+        };
+        websocket.send(JSON.stringify(json));
+    }
+}
+
+function RemoveDevice(deviceMac) {
+    let foundIndex = -1;
+    const devices = actionInfo.payload.settings["devices"];
+    for (let index = 0; index < devices.length; index++) {
+        const device = devices[index];
+        if (device["mac"] === deviceMac) {
+            foundIndex = index;
+        }
+    }
+    if (foundIndex >= 0) {
+        actionInfo.payload.settings["devices"].splice(foundIndex, 1)
+    } else {
+        console.log("No device found with mac ", deviceMac)
+    }
+    UpdateSettings();
+}
+
+function AddDevice(deviceMac, deviceLabel) {
+    device = {"mac": deviceMac, "label": deviceLabel};
+    actionInfo.payload.settings["devices"].push(device);
+    UpdateSettings();
+}
+
+
+function DiscoverDevices() {
+    sendValueToPlugin({ 'type': 'discovery' });
+    let container = document.getElementById('discover-container');
+    let discoveryButton = document.getElementById('discovery');
+    let loader = document.createElement('div');
+    loader.setAttribute('class', 'loader');
+    discoveryButton.setAttribute('disabled', 'disabled');
+    container.append(loader);
+}
