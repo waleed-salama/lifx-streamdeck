574fd913995637e6b722404362f4e20537502666
Sean Smith <sean@wwsean08.com>
2021-05-28 18:39:26 -0700

    Fix G302 (file permission too broad) per vulnerability report 8852788

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var (
 )
 
 func main() {
-	f, err := os.OpenFile("crash_log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("crash_log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 	if err == nil {
 		redirectStderr(f)
 	}
